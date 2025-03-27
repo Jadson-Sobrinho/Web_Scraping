c82a9304e7d37c7970471e077ac694d67aefbf38
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-27 18:34:08 -0300

    Query Refact

--- src/ETL/normalize.py
+++ src/ETL/normalize.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 
 relatorio_cadop_path = ("C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\input\\Relatorio_cadop.csv")
-operadoras_ativas_path = ("C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\output\\1T2024.csv")
+operadoras_ativas_path = ("C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\output\\4T2024.csv")
 
 df_relatorio_cadop = pd.read_csv(relatorio_cadop_path, encoding='utf-8-sig', sep=';')
 df_operadoras_ativas = pd.read_csv(operadoras_ativas_path, encoding='utf-8-sig', sep=';')
@@ -17,14 +17,16 @@ SELECT
     RC.Registro_ANS, 
     RC.Razao_Social,
     OA.DESCRICAO,
-    OA.VL_SALDO_FINAL,
+    (OA.VL_SALDO_INICIAL - OA.VL_SALDO_FINAL) AS VL_DESPESA,
     OA.DATA 
 FROM
     Relatorio_cadop as RC
 JOIN 
     operadoras_ativas as OA ON RC.Registro_ANS = OA.REG_ANS
 WHERE
-    OA.DESCRICAO LIKE "%SINISTROS CONHECIDOS%"
+    OA.DESCRICAO LIKE "%SINISTROS CONHECIDOS OU AVISADOS DE ASSIST%"
+ORDER BY VL_DESPESA
+LIMIT 10;
 """
 resultado = pd.read_sql_query(query, conn)
 
