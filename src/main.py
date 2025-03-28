ca26e361e1d8b7501ff00bf40bffeecbda8375c7
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-28 12:03:53 -0300

    Call rol_procedimentos_transform

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import ETL
+
+def main():
+    
+    df_relatorio_cadop, df_operadoras_ativas = ETL.load_datas()
+    query_result = ETL.process_data(df_relatorio_cadop, df_operadoras_ativas)
+    ETL.save_query_result(query_result)
+
+    #TO-DO: Chamar as funções para zippar o anexo I  -TESTE I
+
+if __name__ == "__main__":
+    main()
