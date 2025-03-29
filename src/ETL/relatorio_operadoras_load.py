cf94ce6c8239e28f7872cd578aa4e99ef23d633f
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-28 22:47:03 -0300

    merge_files

--- src/ETL/relatorio_operadoras_load.py
+++ src/ETL/relatorio_operadoras_load.py
@@ -1,9 +1,29 @@
 import sqlite3
 import pandas as pd
+import glob
+import os
 
-relatorio_cadop_path = ("C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\input\\Relatorio_cadop.csv")
-operadoras_ativas_path = ("C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\output\\Teste3\\Periodos\\Operadoras_ativas_2_ultimos_anos.csv")
-output_dir = ("C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\output")
+relatorio_cadop_path = ("data\\output\\Teste3\\Relatorio\\Relatorio_cadop.csv")
+output_dir = ("data\\output\\Teste3\\Periodos")
+operadoras_ativas_path = ("data\\output\\Teste3\\Periodos\\Operadoras_ativas_2_ultimos_anos.csv")
+
+os.makedirs(output_dir, exist_ok=True)
+
+def merge_files():
+    file_pattern = ("*T*.csv")
+
+    files = glob.glob(os.path.join(output_dir, file_pattern))
+
+    dfs = []
+    for file in files:
+        df = pd.read_csv(file, encoding="utf-8", sep=";", engine="python")
+        dfs.append(df)
+
+    df_consolidated = pd.concat(dfs, ignore_index=True)
+
+    df_consolidated.to_csv(operadoras_ativas_path, index=False, encoding="utf-8-sig", sep=";")
+    
+    print(df_consolidated)
 
 def load_datas():
     df_relatorio_cadop = pd.read_csv(relatorio_cadop_path, encoding='utf-8-sig', sep=';')
