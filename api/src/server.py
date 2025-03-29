433d1f5d6d4837a50d40ebce08d0ee1d7531d901
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-29 20:13:34 -0300

    - chore(app.Route)     - Search the 10 most expenses spended on the last quarter

--- api/src/server.py
+++ api/src/server.py
@@ -4,126 +4,42 @@ import json
 import os
 import sqlite3
 from collections import OrderedDict #Deixa na ordem ao converter para JSON
+import pandas as pd
 
-#TO-DO: Verificar pq os dados de valores estão retornando nulos
+
+#TO-DO: Normalizar as datas em um formato só
 
 
 app = Flask(__name__)
 
 
-DATABASE = "dados.db"
+db_path = "dados.db"
 csv_path = "C:\\Users\\c31f4\\OneDrive\\Desktop\\PROJETOS\\Web_Scraping\\data\\output\\Teste3\\Relatorio_normalizado.csv"
 
 def get_db_connection():
     """Cria e retorna uma conexão com o banco SQLite."""
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row  # Permite acessar as colunas pelo nome
     return conn
 
-def create_table():
-    """Cria a tabela 'empresa' caso ela não exista."""
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS empresa (
-            registro_ans TEXT PRIMARY KEY,
-            cnpj TEXT,
-            razao_social TEXT,
-            modalidade TEXT,
-            logradouro TEXT,
-            numero TEXT,
-            bairro TEXT,
-            cidade TEXT,
-            uf TEXT,
-            cep TEXT,
-            endereco_eletronico TEXT,
-            descricao TEXT,
-            vl_saldo_inicial TEXT,
-            vl_saldo_final TEXT,
-            despesas TEXT
-        )
-        """
-    )
-    conn.commit()
-    conn.close()
 
-def import_csv():
-    """Importa os dados do arquivo CSV para a tabela 'empresa'."""
-    if not os.path.exists(csv_path):
-        print("Arquivo não encontrado")
-        return
+def import_csv(csv_path, db_path, chunk_size=2000):
+    
+    conn = sqlite3.connect(db_path)
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    for chunk in pd.read_csv(csv_path, sep=";", encoding="utf-8-sig", chunksize=chunk_size):
 
-    with open(csv_path, "r", encoding="utf-8") as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=";")
-        for line in reader:
-
-            
-            registro_ans = line.get("Registro_ANS", "").strip()
-            cnpj = line.get("CNPJ", "").strip()
-            razao_social = line.get("Razao_Social", "").strip()
-            modalidade = line.get("Modalidade", "").strip()
-            logradouro = line.get("Logradouro", "").strip()
-            numero = line.get("Numero", "").strip()
-            bairro = line.get("Bairro", "").strip()
-            cidade = line.get("Cidade", "").strip()
-            uf = line.get("UF", "").strip()
-            cep = line.get("CEP", "").strip()
-            endereco_eletronico = line.get("Endereco_eletronico", "").strip()
-            descricao = line.get("DESCRICAO", "").strip()
-            vl_saldo_inicial = line.get("VL_SALDO_INICIAL", "").strip()
-            vl_saldo_final = line.get("VL_SALDO_FINAL", "").strip()
-            despesas = line.get("Despesas", "").strip()
+        #chunk["VL_SALDO_INICIAL"] = pd.to_numeric(chunk["VL_SALDO_INICIAL"], errors="coerce")
+        #chunk["VL_SALDO_FINAL"] = pd.to_numeric(chunk["VL_SALDO_FINAL"], errors="coerce")
 
 
-            try:
-                vl_saldo_inicial = float(vl_saldo_inicial) if vl_saldo_inicial else None
-            except ValueError:
-                vl_saldo_inicial = None
-            try:
-                vl_saldo_final = float(vl_saldo_final) if vl_saldo_final else None
-            except ValueError:
-                vl_saldo_final = None
-            try:
-                despesas = float(despesas) if despesas else None
-            except ValueError:
-                despesas = None
+        chunk.to_sql("empresa", conn, if_exists="append", index=False)
 
-            cursor.execute(
-                """
-                INSERT OR IGNORE INTO empresa (
-                    registro_ans, cnpj, razao_social, modalidade, logradouro, numero,
-                    bairro, cidade, uf, cep, endereco_eletronico, descricao, vl_saldo_inicial, vl_saldo_final, despesas
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """,
-                (
-                    registro_ans,
-                    cnpj,
-                    razao_social,
-                    modalidade,
-                    logradouro,
-                    numero,
-                    bairro,
-                    cidade,
-                    uf,
-                    cep,
-                    endereco_eletronico,
-                    descricao,
-                    vl_saldo_inicial,
-                    vl_saldo_final,
-                    despesas
-                )
-            )
-    conn.commit()
     conn.close()
     print("Dados importados")
 
 
-create_table()
-import_csv()
+import_csv(csv_path, db_path)
 
 @app.route("/api/data", methods=["GET"])
 def obter_dados():
@@ -134,25 +50,29 @@ def obter_dados():
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("""
-                   SELECT registro_ans,
-                    cnpj,
-                    razao_social,
-                    modalidade,
-                    logradouro,
-                    numero,
-                    bairro,
-                    cidade,
-                    uf,
-                    cep,
-                    endereco_eletronico,
-                    descricao,
-                    vl_saldo_inicial,
-                    vl_saldo_final,
-                    despesas 
-                   FROM empresa 
-                    WHERE descricao = "%SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO%"
-                   LIMIT ? OFFSET ?
-                   """, (page_size, offset))
+                    SELECT 
+                        Registro_ANS, 
+                        CNPJ, 
+                        Razao_Social, 
+                        DESCRICAO, 
+                        SUM(VL_SALDO_INICIAL) AS VL_SALDO_INICIAL, 
+                        SUM(VL_SALDO_FINAL) AS VL_SALDO_FINAL,
+                        SUM(VL_SALDO_INICIAL - VL_SALDO_FINAL) AS Despesa,
+                        DATA
+                    FROM empresa
+                    WHERE 
+                        DESCRICAO LIKE "%SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%" 
+                        AND DATA = "2024-10-01"
+                    GROUP BY 
+                        Registro_ANS,
+                        CNPJ,
+                        Razao_Social,  
+                        DESCRICAO,
+                        DATA
+                    HAVING SUM(VL_SALDO_INICIAL - VL_SALDO_FINAL) > 1
+                    ORDER BY Despesa DESC 
+                    LIMIT 10;
+                   """)
     lines = cursor.fetchall()
     conn.close()
 
@@ -160,47 +80,7 @@ def obter_dados():
     json_output = json.dumps(result, ensure_ascii=False, indent=4, sort_keys=False)
     return Response(json_output, content_type="application/json")
 
-@app.route("/api/data/filter", methods=["GET"])
-def filtrar_dados():
-    """Filtra e retorna um registro pelo CNPJ."""
-    cnpj = request.args.get("CNPJ")
-    if not cnpj:
-        return jsonify({"erro": "CNPJ é obrigatório"}), 400
-
-    # Remove caracteres não numéricos do CNPJ
-    cnpj_limpo = ''.join(filter(str.isdigit, cnpj))
-    if len(cnpj_limpo) != 14:
-        return jsonify({"erro": "CNPJ deve conter 14 dígitos"}), 400
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM empresa WHERE cnpj = ?", (cnpj_limpo,))
-    row = cursor.fetchone()
-    conn.close()
-
-    if row is None:
-        return jsonify({"mensagem": "Nenhum registro encontrado para esse CNPJ"}), 404
-
-    result = OrderedDict([
-    ("Registro_ANS", row["registro_ans"]),
-    ("CNPJ", row["cnpj"]),
-    ("Razao_Social", row["razao_social"]),
-    ("Modalidade", row["modalidade"]),
-    ("Logradouro", row["logradouro"]),
-    ("Numero", row["numero"]),
-    ("Bairro", row["bairro"]),
-    ("Cidade", row["cidade"]),
-    ("UF", row["uf"]),
-    ("CEP", row["cep"]),
-    ("Endereco_eletronico", row["endereco_eletronico"]),
-    ("DESCRICAO", row["descricao"]),
-    ("VL_SALDO_INICIAL", row["vl_saldo_inicial"]),
-    ("VL_SALDO_FINAL", row["vl_saldo_final"]),
-    ("Despesas", row["despesas"])
-])
-
-    json_output = json.dumps(result, ensure_ascii=False, indent=4, sort_keys=False)
-    return Response(json_output, content_type="application/json")
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
